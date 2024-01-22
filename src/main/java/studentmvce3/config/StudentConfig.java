2e3ab11c9c5751035eef95f55db787bd35555b0b
unknown <poojithadarshan2131@gmail.com>
2024-01-22 18:12:08 +0530

    final

---
+++ src/main/java/studentmvce3/config/StudentConfig.java
@@ -0,0 +1,17 @@
+package studentmvce3.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "studentmvce3" })
+public class StudentConfig {
+	@Bean
+	public EntityManager getEntityManager() {
+		return Persistence.createEntityManagerFactory("vinod").createEntityManager();
+	}
+}
